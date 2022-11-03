6d3b6c290d92772e755a0c6810173381f062fc1a
Jaume Valiente <jaume.kyc@gmail.com>
2022-11-03 12:54:39 +0100

    primer commit

---
+++ main.js
@@ -0,0 +1,19 @@
+removeKeyboard();
+
+function removeKeyboard(){
+    let keyboard = document.getElementById("keyboard-cont");
+    keyboard.remove();
+}
+
+
+//keyup keydown
+
+array = [];
+addeventListenner("keyup", function(){
+
+    
+
+
+})
+
+
