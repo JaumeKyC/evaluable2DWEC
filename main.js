f7176e86a87abbc8914b9c4c9cbdb5663225be09
Natalia Fernández <91879541+natproject@users.noreply.github.com>
2022-11-04 08:49:24 +0100

    ev2 c.

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-removeKeyboard();
+//removeKeyboard();
 
 function removeKeyboard() {
     let keyboard = document.getElementById("keyboard-cont");
     keyboard.remove(); 
+}
+
+let abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","u","v","w","x","y","z"];
+let letters = [];
+let word = '';
+
+for (let i = 0; i < abc.length; i++) {
+    document.querySelector(`[value = ${abc[i]} ]`).addEventListener('click', function (e) {
+        e.preventDefault();
+        saveLetters(`${abc[i]}`);    
+    })
+}
+
+function saveLetters(letter) {
+    letters.push(letter);
+}
+
+document.querySelector("[value = submit]").addEventListener("click",function (e) {
+    e.preventDefault();
+    word = letters.join('');
+    letters = [];
+    console.log(word)
+    saveInLocalStorage(word);
+})
+
+function saveInLocalStorage(word) {
+    localStorage.setItem((localStorage.length + 1), JSON.stringify(word));
 }
