b99df7d508af90be96d21ac93c8f9e49d3bc1108
RaulHS2000 <raulh2000@hotmail.es>
2022-11-04 08:50:45 +0100

    Funcion E RaulH

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 removeKeyboard();
-
+//Eliminamos temporalmente el teclado
 function removeKeyboard() {
     let keyboard = document.getElementById("keyboard-cont");
-    keyboard.remove(); 
+    keyboard.remove();
 }
 
-//HOLA PITUFO
+let botonPlay = document.getElementsByTagName('h1')[0];
+
+botonPlay.addEventListener('click',letsPlay);
+
+function letsPlay(){
+    let arrayPalabras = ['diego','jaume','natalia','raul'];
+    localStorage.setItem('palabras',arrayPalabras);
+
+    for (let index = 0; index < arrayPalabras.length; index++) {
+        console.log(index);
+    }
+
+    async function getAPI(palabra){
+
+        try {
+            let response = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+index);
+            console.log(response.data);
+            
+        } catch (error) {
+            throw new Error('La petición ha fallado');
+        }
+
+    }
+}
+
+
+
+
+
